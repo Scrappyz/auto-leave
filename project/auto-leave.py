b37fc10ff600e935f4ac0b0333eafc39d4fa8348
Scrappyz <ghostmic3000@gmail.com>
2023-10-24 09:56:38 +0800

    Added display

--- project/auto-leave.py
+++ project/auto-leave.py
@@ -43,35 +43,49 @@ def main():
     pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     current_path = os.path.dirname(__file__)
     
-    parser = argparse.ArgumentParser(prog="Auto Leave")
+    # time.sleep(3)
+    # find(os.path.join(current_path, "resources/test.png"))
+    # pyautogui.screenshot(region=(1781, 353, 60, 37)).save(os.path.join(current_path, "resources/test2.png"))
+    
+    parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--delay", nargs="?", default="5", help="delay before program start", dest="delay")
     parser.add_argument("-t", "--threshold", nargs="?", type=int, default=10, help="the amount before leaving", dest="threshold")
     parser.add_argument("-f", "--frequency", nargs="?", default=10, help="how frequent to check the number of people", dest="frequency")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1.1-alpha")
     
     args = parser.parse_args()
     delay = convertInterval(args.delay)
     threshold = args.threshold
     frequency = convertInterval(args.frequency)
     
-    print("Press Ctrl+C to end program")
     time.sleep(delay)
+    print("[START] Press Ctrl+C to end process")
     
     while True: 
-        ss = pyautogui.screenshot(region=(1770, 436, 60, 27))
+        ss = pyautogui.screenshot(region=(1781, 353, 60, 37))
         people = pytesseract.image_to_string(ss, lang='eng', config="--psm 10 -c tessedit_char_whitelist=0123456789")
-        if not people or int(people) < threshold:
+        if not people:
+            print("[WARNING] Cannot find number of people")
+            time.sleep(frequency)
+            continue
+        
+        print("[INFO] Number of people: " + str(people), end="")
+        
+        if int(people) < threshold:
             leave()
             break
+        
         time.sleep(frequency)
         
-    print("[Program Finished Successfully]")
+    print("[END] Program finished")
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("[Program Terminated]")
+        print("[END] Program terminated")
         
 # Locations (on 1920x1080)
+# people = (1781, 353, 60, 37)
 # people2 = (1770, 436, 60, 27)
     
