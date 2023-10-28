54d10073856c3b06d237e5e362ae964df8daa7d1
Scrappyz <ghostmic3000@gmail.com>
2023-10-28 12:20:49 +0800

    probably be discontinued

--- project/autoleave.py
+++ project/autoleave.py
@@ -5,7 +5,6 @@ import os
 import json
 from pytesseract import pytesseract
 from pynput import mouse, keyboard
-# from PIL import Image, ImageFilter
 
 positions = [[-1, -1], [-1, -1]]
 json_data = {}
@@ -27,13 +26,6 @@ def shift_press(key):
 def getPosition():
     with keyboard.Listener(on_press=shift_press) as listener:
         listener.join()
-        
-def getScreenshotFromPosition(position):
-    ss_pos = (position[0][0], position[0][1], position[1][0] - position[0][0], position[1][1] - position[0][1])
-    return ss_pos
-
-def find(image_path: str):
-    print(pyautogui.locateOnScreen(image_path))
     
 def savePositionToJson(key: str, val: tuple, file_path: str):
     with open(file_path, 'r') as f:
@@ -41,6 +33,19 @@ def savePositionToJson(key: str, val: tuple, file_path: str):
         json_data[key] = val
     with open(file_path, 'w') as f:
         f.write(json.dumps(json_data, indent=4))
+        
+def getPositionFromJson(key: str, file_path: str) -> tuple:
+    with open(file_path, "r") as f:
+        json_data = json.load(f)
+        return json_data[key]
+    
+def getScreenshotFromPosition(position):
+    ss_pos = (position[0][0], position[0][1], position[1][0] - position[0][0], position[1][1] - position[0][1])
+    return ss_pos
+
+def getCenterOfPosition(pos: tuple) -> tuple:
+    center = ((pos[0][0] + pos[1][0]) / 2, (pos[0][1] + pos[1][1]) / 2)
+    return center
 
 def convertInterval(freq) -> float:
     if type(freq) != str:
@@ -69,9 +74,21 @@ def convertInterval(freq) -> float:
     
     return t
 
-def leave():
-    pyautogui.moveTo(1187, 981) # location of leave button in gmeet
+def clickAt(x: int, y: int):
+    pyautogui.moveTo(x, y)
     pyautogui.click()
+    
+def getNumberOfPeople(s: str) -> int:
+    if s.startswith("Contributors"):
+        n = -1
+        for i in reversed(range(len(s))):
+            if not s[i].isdigit():
+                n = i+1
+                break
+        
+        return int(s[n:])
+    
+    return -1
 
 def main():
     pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -89,9 +106,9 @@ def main():
     pos.add_argument("--people", action="store_true", help="save position of number of people", dest="people")
     
     args = parser.parse_args()
-    delay = 0
-    threshold = 0
-    frequency = 1
+    delay = 5
+    threshold = 10
+    frequency = 5
     if args.command == "position":
         if args.button or args.people:
             getPosition()
@@ -105,21 +122,23 @@ def main():
         threshold = args.threshold
         frequency = convertInterval(args.frequency)
     
+    json_path = os.path.join(current_path, "sample.json")
     time.sleep(delay)
     print("[START] Press Ctrl+C to end process")
-    
     while True: 
-        ss = pyautogui.screenshot(region=(1781, 353, 60, 37))
-        people = pytesseract.image_to_string(ss, lang='eng', config="--psm 10 -c tessedit_char_whitelist=0123456789")
-        if not people:
+        ss = pyautogui.screenshot(region=getScreenshotFromPosition(getPositionFromJson("people", json_path)))
+        people = pytesseract.image_to_string(ss, lang='eng', config="--psm 10")
+        number_of_people = getNumberOfPeople(people)
+        if number_of_people < 0:
             print("[WARNING] Cannot find number of people")
             time.sleep(frequency)
             continue
         
         print("[INFO] Number of people: " + str(people), end="")
         
-        if int(people) < threshold:
-            leave()
+        if number_of_people < threshold:
+            click_pos = getCenterOfPosition(getPositionFromJson("button", json_path))
+            clickAt(click_pos[0], click_pos[1])
             break
         
         time.sleep(frequency)
@@ -131,8 +150,4 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("[END] Program terminated")
-        
-# Locations (on 1920x1080)
-# people = (1781, 353, 60, 37)
-# people2 = (1770, 436, 60, 27)
     
--- test/test_autoleave.py
+++ test/test_autoleave.py
@@ -2,11 +2,12 @@ import sys, pathlib, unittest
 
 sys.path.append(str(pathlib.Path(__file__).parents[1].joinpath("project").resolve()))
 
-import auto-leave
+import autoleave
 
 class TestAutoLeave(unittest.TestCase):
-    def test_main(self):
-        self.assertEqual(5, 5)
+    def test_getNumberOfPeople(self):
+        self.assertEqual(autoleave.getNumberOfPeople("Contributors = 1"), 1)
+        self.assertEqual(autoleave.getNumberOfPeople("Contributors = 12"), 12)
         
 if __name__ == "__main__":
     unittest.main()
